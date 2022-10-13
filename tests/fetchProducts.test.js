25d173235abaf04cc7e9255b09f341568958c85e
NettoJM <nettojm86@gmail.com>
2022-10-12 23:50:39 -0300

    corrected a typo, added a white space and semicolons where needed

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,31 +3,32 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
+
   test('testa se fetchProducts é uma função', () => {
     expect(fetchProducts).toBeInstanceOf(Function);
   })
 
   test('testa se fetch foi chamada ao executar fetchProducts com o argumento "computador"', async () => {
     expect.assertions(1);
-    await fetchProducts('computador')
+    await fetchProducts('computador');
     expect(fetch).toHaveBeenCalled();
   }) 
   
   test('testa se fetch usa o endpoint correto ao executar fetchProducts com o argumento "computador"', async () => {
     expect.assertions(1);
-    await fetchProducts('computador')
+    await fetchProducts('computador');
     expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   }) 
   
-  test('testa se o retorno da função fetchProducts com o argumento "computador" é correta', async () => {
+  test('testa se o retorno da função fetchProducts com o argumento "computador" é correto', async () => {
     expect.assertions(1);
-    const fetchedProducts = await fetchProducts('computador')
-    expect(fetchedProducts).toEqual(computadorSearch)
+    const fetchedProducts = await fetchProducts('computador');
+    expect(fetchedProducts).toEqual(computadorSearch);
   })
 
   test('testa se, ao chamar a função fetchProducts sem argumento, retorna o erro correto', async () => {
     expect.assertions(1);
-    expect(await fetchProducts()).toThrowError('You must provide an url')
+    expect(await fetchProducts()).toThrowError('You must provide an url');
   })
   
 });
