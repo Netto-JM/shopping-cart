9dcbf329278ea10cf3f8d168538866867dfbe9be
NettoJM <nettojm86@gmail.com>
2022-10-13 01:11:46 -0300

    implemented fetchProducs helper

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,5 +1,10 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (qry) => {
+  if (!qry) throw new Error('You must provide an url');
+  const QUERY = 'computador';
+  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`;
+  const result = await fetch(endpoint);
+  const data = await result.json();
+  return data;
 };
 
 if (typeof module !== 'undefined') {
