e8b08caf874f932f1d1a7d7330e93737bb7d88b7
NettoJM <nettojm86@gmail.com>
2022-10-14 15:56:21 -0300

    testing if saveCartItems is a function

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,6 +4,9 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+
+  it('testa se saveCartItems é uma função', () => {
+    expect(saveCartItems).toBeInstanceOf(Function);
+  })
+
 });
