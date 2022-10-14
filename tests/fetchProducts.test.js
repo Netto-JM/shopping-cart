985c675bbfc2e16bce50d1061184a800e4c55355
NettoJM <nettojm86@gmail.com>
2022-10-14 11:15:26 -0300

    made it semantically more readable

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -4,29 +4,29 @@ const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
 
-  test('testa se fetchProducts é uma função', () => {
+  it('testa se fetchProducts é uma função', () => {
     expect(fetchProducts).toBeInstanceOf(Function);
   })
 
-  test('testa se fetch foi chamada ao executar fetchProducts com o argumento "computador"', async () => {
+  it('testa se fetch foi chamada ao executar fetchProducts com o argumento "computador"', async () => {
     expect.assertions(1);
     await fetchProducts('computador');
     expect(fetch).toHaveBeenCalled();
   }) 
   
-  test('testa se fetch usa o endpoint correto ao executar fetchProducts com o argumento "computador"', async () => {
+  it('testa se fetch usa o endpoint correto ao executar fetchProducts com o argumento "computador"', async () => {
     expect.assertions(1);
     await fetchProducts('computador');
     expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   }) 
   
-  test('testa se o retorno da função fetchProducts com o argumento "computador" é correto', async () => {
+  it('testa se o retorno da função fetchProducts com o argumento "computador" é correto', async () => {
     expect.assertions(1);
     const fetchedProducts = await fetchProducts('computador');
     expect(fetchedProducts).toEqual(computadorSearch);
   })
 
-  test('testa se, ao chamar a função fetchProducts sem argumento, retorna o erro correto', async () => {
+  it('testa se, ao chamar a função fetchProducts sem argumento, retorna o erro correto', async () => {
     expect.assertions(1);
     await expect(fetchProducts())
     .rejects
