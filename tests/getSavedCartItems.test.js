db7d556a9f6353abd8fe4400209a078ecf08389b
NettoJM <nettojm86@gmail.com>
2022-10-14 15:57:49 -0300

    testing if getSavedCartItems is a function

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,9 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+
+  it('testa se getSavedCartItems é uma função', () => {
+    expect(getSavedCartItems).toBeInstanceOf(Function);
+  })
+
 });
