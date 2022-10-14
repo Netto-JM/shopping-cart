87cc66a0459352fdccdc40b781d63eef69143e1b
NettoJM <nettojm86@gmail.com>
2022-10-14 11:13:42 -0300

    made it functional

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,5 @@
-const fetchProducts = async (qry) => {
-  if (!qry) throw new Error('You must provide an url');
-  const QUERY = 'computador';
+const fetchProducts = async (QUERY) => {
+  if (!QUERY) throw new Error('You must provide an url');
   const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`;
   const result = await fetch(endpoint);
   const data = await result.json();
