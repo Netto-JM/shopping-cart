e02dc2066f483c176e43dce23d0a8f27db1a5959
NettoJM <nettojm86@gmail.com>
2022-10-14 11:19:50 -0300

    testing if fetch was called when calling fetchItem with "MLB1615760527" as an argument

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -7,4 +7,11 @@ describe('2 - Teste a função fetchItem', () => {
   it('testa se fetchItem é uma função', () => {
     expect(fetchItem).toBeInstanceOf(Function);
   })
+
+  it('testa se fetch foi chamada ao executar fetchItem com o argumento "MLB1615760527"', async () => {
+    expect.assertions(1);
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  })
+  
 });
