b2f3ca9ada5d36ea892fde1f2bdebf1b2a7f6bec
NettoJM <nettojm86@gmail.com>
2022-10-14 12:58:20 -0300

    fetchItem function implemented

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,9 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (ItemID) => {
+  if (!ItemID) throw new Error('You must provide an url');
+  const endpoint = `https://api.mercadolibre.com/items/${ItemID}`;
+  const result = await fetch(endpoint);
+  const data = await result.json();
+  return data;
 };
 
 if (typeof module !== 'undefined') {
