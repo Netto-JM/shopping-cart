d8554ff0c3c5eb2dc0fdc0d4fcf8b54c9824dd1f
NettoJM <nettojm86@gmail.com>
2022-10-15 00:02:38 -0300

    corrected type

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -16,7 +16,7 @@ describe('3 - Teste a função saveCartItems', () => {
 
   it('testa se, ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro a chave "cartItems" e o segundo sendo o valor passado como argumento para saveCartItems', () => {
     saveCartItems('someCartItem');
-    expect(localStorage.setItem).toBeCalled('cartItems', 'someCartItem');
+    expect(localStorage.setItem).toBeCalledWith('cartItems', 'someCartItem');
   })
 
 });
